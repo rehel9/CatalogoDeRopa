b46d4d8fbb2abfdc70b055c1063bb4eb7917dab9
Luis Leonardo <78172097+rehel9@users.noreply.github.com>
2025-01-03 14:30:23 -0600

    Erased code autocreated

--- main.cpp
+++ main.cpp
@@ -8,22 +8,6 @@ int main( )
 {
 	initwindow( 640 , 480 , "WinBGIm" );
 	
-	
-	
-    
-	outtextxy( 0 , 0 , "Hello WinBGIm!" );
-    
-	bgiout << "Hello WinBGIm-Stream!" ;
-	outstreamxy( 0 , 15 ); 
-	
-    
-    
-    
-    
-	while( !kbhit() ); 
-  
-    
-    
 	closegraph( );
     
 	return( 0 );
